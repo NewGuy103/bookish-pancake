f13438257a6338d4968e82cfb85a33e06953ba05
NewGuy103 <122655683+NewGuy103@users.noreply.github.com>
2023-02-28 20:57:40 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,1 @@
-local parts = {}
 
-while wait() do
-  task.spawn(function()
-    for i = 1, 1000000 do
-      local newPart = Instance.new("Part")
-      newPart.Name = tostring(#parts + 1)
-      newPart.Size = Vector3.new(2, 2, 2)
-      newPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-      newPart.Anchored = true
-      newPart.CanCollide = true
-      newPart.Color = Color3.new(1, 0, 0)
-      newPart.Parent = workspace
-
-      table.insert(parts, newPart)
-    end
-  end)
-end
-print("trolled hahahahahaha")
